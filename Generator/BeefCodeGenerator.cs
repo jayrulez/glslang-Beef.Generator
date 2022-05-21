8dedcd55cc0f4a191a9c5063b87109e6c610562d
Robert Campbell <waprave@gmail.com>
2022-05-21 05:20:57 -0400

    Started generator

---
+++ Generator/BeefCodeGenerator.cs
@@ -0,0 +1,17 @@
+﻿using CppAst;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace glslang_Beef.Generator
+{
+    public class BeefCodeGenerator
+    {
+        public static int Generate(CppCompilation compilation, string projectNamespace, string v)
+        {
+            return 0;
+        }
+    }
+}
---
+++ Generator/Program.cs
@@ -0,0 +1,42 @@
+﻿using CppAst;
+using System;
+using System.IO;
+
+namespace glslang_Beef.Generator
+{
+    public class Program
+    {
+        private static string glslangSourceDir = "../../../../../glslang";
+        private static string OutputDir = "../../../../../glslang-Beef/glslang-Beef/src/Generated";
+        private static string ProjectNamespace = "glslang_Beef";
+
+        public static int Main(string[] args)
+        {
+            var options = new CppParserOptions
+            {
+                ParseMacros = true,
+            };
+
+            CppCompilation compilation = CppParser.ParseFile(Path.Combine(glslangSourceDir,"glslang","Include", "glslang_c_interface.h"), options);
+
+            // Print diagnostic messages
+            if (compilation.HasErrors)
+            {
+                foreach (var message in compilation.Diagnostics.Messages)
+                {
+                    if (message.Type == CppLogMessageType.Error)
+                    {
+                        var currentColor = Console.ForegroundColor;
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine(message);
+                        Console.ForegroundColor = currentColor;
+                    }
+                }
+
+                return 0;
+            }
+
+            return BeefCodeGenerator.Generate(compilation, ProjectNamespace, Path.GetFullPath(OutputDir));
+        }
+    }
+}
